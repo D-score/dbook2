37fa2544e28ac7b82c2b2024020a11267b4de08c
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-07-20 17:48:51 +0200

    Rerun the docs didn't work with bookdown 0.20, so we use 0.19

--- R/before_book_script.R
+++ R/before_book_script.R
@@ -8,3 +8,6 @@ knit_hooks$set(document = function(x) {
 # R options
 options(na.action = na.fail, width = 64, digits = 3, scipen = 6,
         continue = "  ")
+
+# see https://stackoverflow.com/questions/62578811/error-input-files-not-all-in-same-directory-please-supply-explicit-wd
+options(bookdown.render.file_scope = FALSE)
